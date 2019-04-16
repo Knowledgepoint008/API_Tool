25fb528d7de06e32f5a58e16120a35314d190e6c
Knowledgepoint008 <48924806+Knowledgepoint008@users.noreply.github.com>
2019-04-15 23:14:53 -0400

    Flask with BootStrap API Tool

--- test.py
+++ test.py
@@ -1,29 +1,19 @@
-'''
-Created on Mar 25, 2019
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
-@author: Chairman
-'''
-from httplib2 import Http
-from urllib.parse import urlencode
-from flask import Flask,render_template,request,jsonify
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
 
 @app.route('/')
-def index():
-    return render_template("TestIndex.html")
+def hello_world():
+    return render_template('index.html')
 
-@app.route('/test')
-def testResult():
-    return "TestIndex.html"
 
-@app.route('/square/', methods=['POST'])
-def square():
-    urlString = request.form.get('urlString')
-    methodType = request.form.get('methodType')
-    httpTest = Http();
-    resp, content = httpTest.request(urlString, methodType, urlencode(""))
-    data = str(resp.status)+" "+resp.reason
-    return data
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
-if __name__ == "__main__":
-    app.run(debug = True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
